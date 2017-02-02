d3d45e035a4d59c9a0ff2102bdd95a6967de5a6b
artur.kordzik <icandoit.ak@outlook.com>
2017-02-02 11:14:15 +0100

    additional BLL tests

--- WebAPI/WebAPI.BLL.Tests/Facades/ValuesFcdTests.cs
+++ WebAPI/WebAPI.BLL.Tests/Facades/ValuesFcdTests.cs
@@ -1,4 +1,5 @@
-﻿using Shouldly;
+﻿using JsonPatch;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,8 @@ using WebAPI.Common.Exceptions;
 using WebAPI.Contracts.BLL;
 using WebAPI.Contracts.DAL;
 using WebAPI.Mocks;
+using WebAPI.Model.Dto.Update;
+using WebAPI.Model.Dto.Write;
 using WebAPI.Model.SearchParams;
 using Xunit;
 
@@ -18,6 +21,11 @@ namespace WebAPI.BLL.Tests.Facades
     {
         private readonly IValuesFcd _fcd;
         private readonly IValuesRepository _repo;
+        private readonly ValueWriteDto _newValue = new ValueWriteDto
+        {
+            Name = "new value",
+            Description = "desc"
+        };
 
         public ValuesFcdTests()
         {
@@ -41,6 +49,58 @@ namespace WebAPI.BLL.Tests.Facades
             var func = new Func<Task>(() => _fcd.Get(incorrectId));
 
             Assert.ThrowsAsync<NotFoundException>(func);
+        }
+
+        [Fact]
+        public void create_value_adds_value()
+        {
+            var initialCount = _repo.Get(new BaseSearchParams()).Result.Items.Count();
+            var created = _fcd.Create(_newValue).Result;
+            var laterCount = _repo.Get(new BaseSearchParams()).Result.Items.Count();
+
+            laterCount.ShouldBe(initialCount + 1);
+
+            created = _fcd.Get(created.Id).Result;
+        }
+
+        [Fact]
+        public void partial_update_updates_value()
+        {
+            var newName = "updated name";
+            var updateId = 4;
+            var model = new JsonPatchDocument<ValueUpdateDto>();
+            model.Add("Name", newName);
+
+            var updated = _fcd.Update(updateId, model).Result;
+
+            updated.Name.ShouldBe(newName);
+
+            var value = _fcd.Get(updateId).Result;
+
+            value.Name.ShouldBe(newName);
+        }
+
+        [Fact]
+        public void full_update_replaces_value()
+        {
+            var updateId = 4;
+            var updated = _fcd.Update(updateId, _newValue).Result;
+            var value = _fcd.Get(updateId).Result;
+
+            value.Name.ShouldBe(_newValue.Name);
+            value.Description.ShouldBe(_newValue.Description);
+        }
+
+        [Fact]
+        public void delete_values_deletes_value()
+        {
+            var deleteId = 4;
+            var func = new Func<Task>(() => _fcd.Get(deleteId));
+
+            _fcd.Delete(deleteId).ContinueWith((t) =>
+            {
+                Assert.ThrowsAsync<NotFoundException>(func);
+            });
         }
     }
 }
