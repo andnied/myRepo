453917be54497fbf85bef341ed00f1974519219c
artur.kordzik <icandoit.ak@outlook.com>
2017-02-16 16:49:03 +0100

    OWIN startup project commit

---
+++ WebAPI/WebAPI.API/App_Start/ApiMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebAPI.API.App_Start
+{
+    public class ApiMiddleware
+    {
+        private readonly Func<IDictionary<string, object>, Task> _next;
+
+        public ApiMiddleware(Func<IDictionary<string, object>, Task> next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            var owinContext = new OwinContext(env);
+            
+            await _next(env);
+        }
+    }
+}
---
+++ WebAPI/WebAPI.API/Filters/ModelValidationAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebAPI.API.Filters
+{
+    public sealed class ModelValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext.ModelState.IsValid)
+            {
+                return;
+            }
+
+            actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, actionContext.ModelState);
+        }
+    }
+}
